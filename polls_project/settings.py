cb9acb6ec99231432089c8757c4ca86c02371556
Twinkle <twinkle@MacBook-Pro.local>
2022-03-19 21:18:42 +0530

    Altered Allowed Hosts

--- polls_project/settings.py
+++ polls_project/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'voltsverse-polls.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+                 '0.0.0.0', 'voltsverse-polls.herokuapp.com']
 
 # Application definition
 
